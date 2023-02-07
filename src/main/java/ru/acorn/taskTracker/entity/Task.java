4f6ca499dd504b05633a6868b90fa53a1e2b27b4
AntonAcorn <ShustikovAnimation@gmail.com>
2023-02-07 14:16:26 +0300

    Adding Task entity

--- src/main/java/ru/acorn/taskTracker/entity/Task.java
+++ src/main/java/ru/acorn/taskTracker/entity/Task.java
@@ -1,18 +1,30 @@
 package ru.acorn.taskTracker.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
+@Table(name = "task")
+@Setter
+@Getter
+@EqualsAndHashCode(exclude = "id")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty
     private String taskName;
+
+    @NotEmpty
     private String taskDescription;
 
-
-
+    @ManyToOne
+    @JoinColumn(name = "task_id", referencedColumnName = "id")
+    private Project project;
 }
