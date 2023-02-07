fc4e91813628a79bc5a1fcaf056e56f43a50bd1e
AntonAcorn <ShustikovAnimation@gmail.com>
2023-02-07 14:12:30 +0300

    Creating ProjectEntity

---
+++ src/main/java/ru/acorn/taskTracker/entity/Task.java
@@ -0,0 +1,18 @@
+package ru.acorn.taskTracker.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String taskName;
+    private String taskDescription;
+
+
+
+}
