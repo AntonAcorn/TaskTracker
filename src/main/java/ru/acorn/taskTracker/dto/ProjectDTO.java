bbbb186e40e4dedd72e52e8fdfb206ebdd4aa4c8
AntonAcorn <ShustikovAnimation@gmail.com>
2023-02-08 20:12:31 +0300

    finish

--- src/main/java/ru/acorn/taskTracker/dto/ProjectDTO.java
+++ src/main/java/ru/acorn/taskTracker/dto/ProjectDTO.java
@@ -1,5 +1,6 @@
 package ru.acorn.taskTracker.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,8 +23,10 @@ public class ProjectDTO {
 
     private String projectName;
 
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime startTimeOfProject;
 
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime completionDate;
 
     private ProjectStatus status;
